5dec8b341fe3ae8e505dde4e20feba957abd87f9
AndreiTotoro <andreitotoro@gmail.com>
2022-07-26 21:12:04 +0300

    finish exercise 1.5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
-      <Header name={course} />
-      <Content part1 = {part1} exercises1 = {exercises1} part2 = {part2} exercises2 = {exercises2} part3 = {part3} exercises3 = {exercises3} />
-      <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3} />
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
 
 const Header = (props) =>(
-<h1>{props.name}</h1>
+<h1>{props.course}</h1>
 )
 
 const Part = (props) => (
@@ -26,14 +34,14 @@ const Part = (props) => (
 
 const Content = (props) => (
   <>
-      <Part partName = {props.part1} partExercises = {props.exercises1} />
-      <Part partName = {props.part2} partExercises = {props.exercises2} />
-      <Part partName = {props.part3} partExercises = {props.exercises3} />
+      <Part partName = {props.parts[0].name} partExercises = {props.parts[0].exercises} />
+      <Part partName = {props.parts[1].name} partExercises = {props.parts[1].exercises} />
+      <Part partName = {props.parts[2].name} partExercises = {props.parts[2].exercises} />
   </>
 )
 
 const Total = (props) => (
-  <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+  <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 )
 
 
