dde6339ef5dc608f7a23b9007ab0c8c888145fb4
AndreiTotoro <andreitotoro@gmail.com>
2022-08-14 23:10:07 +0300

    Fixed key error

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -2,7 +2,6 @@ import React from "react";
 
 const Sum = ({parts}) => {
   const total = parts.reduce((sum, part) => {
-    console.log(sum, part)
     return sum + part.exercises
   }, 0)
   return(
